d10edbfb3fa01cca47e71f8705f77493feb45958
bunny-pc <79827291+bunny-pc@users.noreply.github.com>
2021-03-12 10:46:08 +0530

    Update AddDataIntoDatabase.java

--- JPA-Hibernate-LAB/jpa-hybernate-lab2/src/main/java/com/cg/jhl2/ui/AddDataIntoDatabase.java
+++ JPA-Hibernate-LAB/jpa-hybernate-lab2/src/main/java/com/cg/jhl2/ui/AddDataIntoDatabase.java
@@ -15,42 +15,43 @@ public class AddDataIntoDatabase {
 	public static void main(String[] args) {
 		EntityManager em=JPAUtil.getEntityManager();
 		
-		Book b1=new Book(1001L,"Mathmatics",1600.0);
-		Book b2=new Book(1002L,"JAVA",400.0);
-		Book b3=new Book(1003L,"CProgramming",300.0);
-		Book b4=new Book(1004L,"Physics",1200.0);
-		Book b5=new Book(1005L,"Chemistry",600.0);
-		Book b6=new Book(1006L,"Biology",1000.0);
-		Book b7=new Book(1007L,"Economics",800.0);
+		Book b1=new Book(1001L,"Mathmatics",100.0);
+		Book b2=new Book(1002L,"JAVA",600.0);
+		Book b3=new Book(1003L,"CProgramming",900.0);
+		Book b4=new Book(1004L,"Physics",1400.0);
+		Book b5=new Book(1005L,"Chemistry",1600.0);
+		Book b6=new Book(1006L,"Biology",1300.0);
+		Book b7=new Book(1007L,"Economics",1800.0);
 		
 		
-		Author a1=new Author(1001L,"Venkata");
-		Author a2=new Author(1002L,"Sai");
-		Author a3=new Author(1003L,"SaiKumar");
-		Author a4=new Author(1004L,"Naveen");
+		Author a1=new Author(1001L,"bunny");
+		Author a2=new Author(1002L,"surya");
+		Author a3=new Author(1003L,"harshitha");
+		Author a4=new Author(1004L,"charitra");
 		
-		Set<Book> venkataBooks=new LinkedHashSet<>();
-		venkataBooks.add(b1);
-		venkataBooks.add(b2);
-		venkataBooks.add(b4);
+		Set<Book> bunnyBooks=new LinkedHashSet<>();
+		bunnyBooks.add(b1);
+		bunnyBooks.add(b2);
+		bunnyBooks.add(b4);
 		
-		Set<Book> saiBooks=new LinkedHashSet<>();
-		saiBooks.add(b7);
-		saiBooks.add(b1);
+		Set<Book> suryaBooks=new LinkedHashSet<>();
+		suryaBooks.add(b7);
+		suryaBooks.add(b1);
 		
-		Set<Book> saiKumarBooks=new LinkedHashSet<>();
-		saiKumarBooks.add(b6);
-		saiKumarBooks.add(b5);
+		Set<Book> harshithaBooks=new LinkedHashSet<>();
+		harshitha
+			Books.add(b6);
+		harshithaBooks.add(b5);
 		
-		Set<Book> naveenBooks=new LinkedHashSet<>();
-		naveenBooks.add(b3);
-		naveenBooks.add(b4);
-		naveenBooks.add(b2);
+		Set<Book> charitraBooks=new LinkedHashSet<>();
+		charitraBooks.add(b3);
+		charitraBooks.add(b4);
+		charitraBooks.add(b2);
 		
-		a1.setBooks(venkataBooks);
-		a2.setBooks(saiBooks);
-		a3.setBooks(saiKumarBooks);
-		a4.setBooks(naveenBooks);
+		a1.setBooks(bunnyBooks);
+		a2.setBooks(suryaBooks);
+		a3.setBooks(harshithaBooks);
+		a4.setBooks(charitraBooks);
 		
 		
 		EntityTransaction txn=em.getTransaction();
